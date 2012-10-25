45e0f4d4e8add8e419b3121acdc326c7fc97852c
Vincent Puydoyeux <v.puydoyeux@gmail.com>
2012-10-25 15:46:14 +0200

    Add console mode

--- src/FooPileRPL.java
+++ src/FooPileRPL.java
@@ -1,27 +1,54 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 
 public class FooPileRPL {
 
-
+	static boolean tryParseInt(String st)  
+	{  
+	     try  {  
+	         Integer.parseInt(st);  
+	         return true;  
+	      } catch(NumberFormatException e) {  
+	          return false;  
+	      }  
+	}
+	
 	public static void main(String[] args) {
-		
+	
+		String line, token;
+		StringTokenizer st;
 		PileRPL pile = new PileRPL();
-
+		Scanner sc = new Scanner(System.in);
+		
 		try {
-			pile.push(new ObjetEmpilable(1));
-			pile.push(new ObjetEmpilable(2));
-			pile.add();
-			pile.push(new ObjetEmpilable(5));
-			pile.sub();
-			pile.push(new ObjetEmpilable(5));
-			pile.mult();
-			pile.push(new ObjetEmpilable(0));
-			pile.div();
-			
+			while (!(line = sc.nextLine()).equals("")){
+				st = new StringTokenizer(line);
+		
+				while (st.hasMoreTokens()) {
+					  token = st.nextToken();
+					  
+					  if (tryParseInt(token))
+						  pile.push(new ObjetEmpilable(Integer.parseInt(token)));
+					  else if (token.equals("+"))
+						  pile.add();
+					  else if (token.equals("-"))
+						  pile.sub();
+					  else if (token.equals("*"))
+						  pile.mult();
+					  else if (token.equals("/"))
+						  pile.div();					  
+					}
+			}	
 			System.out.println(pile);
 			
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
+		finally {
+			sc.close();
+		}
 	}
+	
 }
